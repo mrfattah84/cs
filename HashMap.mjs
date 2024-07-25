687f7816b8de79a008a4fc33ee97e7966c0fdbee
mrfattah84 <148151611+mrfattah84@users.noreply.github.com>
2024-07-25 16:29:49 +0330

    hash map edited

--- HashMap.mjs
+++ HashMap.mjs
@@ -1,101 +1,112 @@
 export default class HashMap {
   constructor() {
-    this.buckets = new Array(16);
-  }
-
-  insert(index, value) {
-    if (index < 0 || index >= this.buckets.length) {
-      throw new Error('Trying to access index out of bound');
-    } else {
-      this.buckets[index] = value;
-    }
+    this.buckets = Array.from(Array(16), () => new Array());
   }
 
   hash(key) {
     let hashCode = 0;
-
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
       hashCode %= this.buckets.length;
     }
-
     return hashCode;
   }
 
   set(key, value) {
-    this.buckets.insert(this.hash(key), [key, value]);
+    let bucket = this.buckets[this.hash(key)];
+    let keyFound = false;
+    bucket.forEach((data) => {
+      if (data[0] == key) {
+        data[1] = value;
+        keyFound = true;
+      }
+    });
+    if (!keyFound) {
+      bucket.push([key, value]);
+    }
+    if (this.length() > this.buckets.length * 0.75) {
+      let bucketsCopy = [...this.buckets];
+      this.buckets = Array.from(
+        Array(this.buckets.length * 2),
+        () => new Array()
+      );
+      bucketsCopy.forEach((bucket) => {
+        bucket.forEach((e) => {
+          this.set(e[0], e[1]);
+        });
+      });
+    }
   }
 
   get(key) {
-    const bucket = this.buckets[this.hash(key)];
-    return bucket !== undefined ? bucket[1] : null;
+    let bucket = this.buckets[this.hash(key)];
+    for (let i = 0; i < bucket.length; i++) {
+      const e = bucket[i];
+      if (key == e[0]) {
+        return e[1];
+      }
+    }
+    return null;
   }
 
   has(key) {
-    const bucket = this.buckets[this.hash(key)];
-    if (bucket !== undefined && bucket[0] === key) {
-      return true;
-    } else {
-      return false;
-    }
+    return this.get(key) === null ? false : true;
   }
 
   remove(key) {
-    const bucket = this.buckets[this.hash(key)];
-    if (bucket !== undefined && bucket[0] === key) {
-      delete this.buckets[this.hash(key)];
-      return true;
-    } else {
-      return false;
+    let bucket = this.buckets[this.hash(key)];
+    for (let i = 0; i < bucket.length; i++) {
+      const e = bucket[i];
+      if (key == e[0]) {
+        bucket.splice(i, 1);
+        return true;
+      }
     }
+    return false;
   }
 
   length() {
-    let c = 0;
-    for (let i = 0; i < this.buckets.length; i++) {
-      const element = this.buckets[i];
-      if (element !== undefined) c++;
-    }
-    return c;
+    let i = 0;
+    this.buckets.forEach((bucket) => {
+      bucket.forEach((e) => {
+        i++;
+      });
+    });
+    return i;
   }
 
   clear() {
-    this.buckets = new Array(this.buckets.length);
+    this.buckets = Array.from(Array(16), () => new Array());
   }
 
   keys() {
     let keys = [];
-    this.buckets.forEach((e) => {
-      if (e !== undefined) keys.push(e[0]);
+    this.buckets.forEach((bucket) => {
+      bucket.forEach((e) => {
+        keys.push(e[0]);
+      });
     });
     return keys;
   }
 
   values() {
     let values = [];
-    this.buckets.forEach((e) => {
-      if (e !== undefined) values.push(e[1]);
+    this.buckets.forEach((bucket) => {
+      bucket.forEach((e) => {
+        values.push(e[1]);
+      });
     });
     return values;
   }
 
   entries() {
-    let res = [];
-    this.buckets.forEach((e) => {
-      if (e !== undefined) res.push([e[0], e[1]]);
+    let entries = [];
+    this.buckets.forEach((bucket) => {
+      bucket.forEach((e) => {
+        entries.push(e);
+      });
     });
-    return res;
+    return entries;
   }
 }
-
-let table = new HashMap();
-table.set('amir', 'fattah');
-table.set('niusha', 'fattah');
-console.log(table.get('amir'));
-console.log(table.buckets);
-console.log(table.has('amir'));
-console.log(table.length());
-console.log(table.keys());
-console.log(table.values());
-console.log(...table.entries());
